281a18adee3a8cb0db123346c662318e1a39adec
Sebastian Britez <sofmarsistema@gmail.com>
2025-02-25 06:40:01 -0300

    hola

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const api_url = 'https://node.gaesa.com.py:8080/api/'
+export const api_url = 'https://node.sofmar.com.py:4001/api/'
 
 //export const api_url = 'https://localhost:4000/api/'
 //export const  api_url = 'https://db.sofmar.com.py:4009/api/' //acricolor
@@ -17,13 +17,13 @@ export const api_url = 'https://node.gaesa.com.py:8080/api/'
 /* Aqui va la fecha del release, la version y la db */
 
 
-export const version = '0.9.9'
+export const version = '1.0'
 
 
 
 
 
 
-export const fechaRelease = '24/02/2025'
-export const db = 'gaesa'
+export const fechaRelease = '25/02/2025'
+export const db = 'zimer'
 export const userName = sessionStorage.getItem('userName')
